4358b2d0a8daf7d920f229cf6dffc15adb37121d
Lukasz Wilinski <159558563+LukaszWilinski95@users.noreply.github.com>
2024-02-17 13:51:39 +0000

    Update (Python) Blackjack.py

--- (Python) Blackjack.py
+++ (Python) Blackjack.py
@@ -3,11 +3,8 @@ THIS IS A CODE WRITTEN WITH SPECIFIED OBJECTIVES (GAME RULES AND CLASSES TO DEFI
 '''
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')  # Suits for regular deck of cards
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King',
-         'Ace')  # Ranks for regular deck of cards
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
-          'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
-# Values for each rank, special cards has value of 10 in BlackJack, while Aces can be either 1 or 11
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')  # Ranks for regular deck of cards
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11} # Values for each rank, special cards has value of 10 in BlackJack, while Aces can be either 1 or 11
 
 import random  # For shuffling the deck later
 
